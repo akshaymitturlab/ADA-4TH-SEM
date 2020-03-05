1de2afd397da321563ab4ea2233837f8a712c51e
Akshay Mittur <53973656+akshaymitturlab@users.noreply.github.com>
2020-03-05 13:00:11 +0530

    Week 5

--- dfsconnected.cpp
+++ dfsconnected.cpp
@@ -1,40 +1,34 @@
-#include <bits/stdc++.h> 
+#include<iostream>
 
-using namespace std; 
-  
-vector<int> gr[100]; 
-  
-bool vis[100]; 
-  
-void Add_edge(int u, int v) { 
-    gr[u].push_back(v); 
-} 
+using namespace std;
 
-void dfs(int x) { 
-    vis[x] = true; 
-  
-    for (auto i : gr[x]) if (!vis[i]) dfs(i); 
-} 
-  
-bool Is_Connected(int n) { 
-    memset(vis, false, sizeof vis); 
-    dfs(1); 
-  
-    for (int i = 1; i <= n; i++) if (!vis[i]) return false; 
-  
-    return true; 
-} 
-  
-int main() { 
-    int n = 4; 
-  
-    Add_edge(1, 2); 
-    Add_edge(1, 3); 
-    Add_edge(2, 3); 
-    //Add_edge(3, 4); 
-  
-    if (Is_Connected(n)) cout << "Yes"; 
-    else cout << "No"; 
-  
-    return 0; 
+int graph[5][5] = {{0, 1, 1, 0, 0},
+{1, 0, 1, 1, 0},
+{1, 1, 0, 1, 1},
+{0, 1, 1, 0, 1},
+{0, 0, 1, 1, 0}};
+
+void dfs(int u, bool visited[]) {
+   visited[u] = true;
+   for(int v = 0; v < 5; v++) {
+      if(graph[u][v]) {
+         if(!visited[v])
+            dfs(v, visited);
+      }
+   }
+}
+
+bool isConnected() {
+   bool *vis = new bool[5];
+
+   for(int u; u < 5; u++) {
+      for(int i = 0; i < 5; i++) vis[i] = false;
+      dfs(u, vis);
+      for(int i = 0; i < 5; i++) if(!vis[i]) return false;
+   }
+   return true;
+}
+int main() {
+   if(isConnected()) cout << "The Graph is connected.";
+   else cout << "The Graph is not connected.";
 }
