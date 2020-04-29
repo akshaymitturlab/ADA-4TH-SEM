549aed24642de4a7dd87170d6ab3f9f13b97d360
Akshay Mittur <53973656+akshaymitturlab@users.noreply.github.com>
2020-04-29 19:35:31 +0530

    week 18

--- heapsort.cpp
+++ heapsort.cpp
@@ -14,7 +14,7 @@ void heapify(int arr[], int n, int i) {
 } 
 void heapSort(int arr[], int n) {  
     for (int i = n / 2 - 1; i >= 0; i--) heapify(arr, n, i); 
-    for (int i=n-1; i>0; i--) {  
+    for (int i = n - 1; i > 0; i--) {  
         swap(arr[0], arr[i]); 
         heapify(arr, i, 0); 
     } 
